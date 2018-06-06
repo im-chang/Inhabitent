4a67b999cb16e61abadfdaf21a0213d0a63f6f89
im-chang <mchang0718@gmail.com>
2018-06-06 16:40:29 -0700

    add archive posts

--- themes/inhabitent/archive.php
+++ themes/inhabitent/archive.php
@@ -6,7 +6,7 @@
  */
 
 get_header(); ?>
-	<div class="shop-stuffs">
+	<div class="archives">
 	<div id="primary" class="content-shop-area">
 		<main id="main" class="site-main" role="main">
 
@@ -18,23 +18,11 @@ get_header(); ?>
 					the_archive_description( '<div class="taxonomy-description">', '</div>' );
 				?>
 			</header><!-- .page-header -->
-			<div class="shop-items">
+			<div class="archive-items">
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                <header class="entry-header">
-                    <?php if ( has_post_thumbnail() ) : ?>
-                        <?php the_post_thumbnail( 'full' ); ?>
-                    <?php endif; ?>
-
-                    <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a>' ); ?>
-					<span class="individual-price">
-                    <?php echo CFS()->get( 'price' ); ?>
-					</span></h2>
-                </header><!-- .entry-header -->
-
-            </article><!-- #post-## -->
+				<?php get_template_part( 'template-parts/content' ); ?>
 
 			<?php endwhile; ?>
 
@@ -45,11 +33,14 @@ get_header(); ?>
 			<?php get_template_part( 'template-parts/content', 'none' ); ?>
 
 		<?php endif; ?>
+		
+		
 </div>
 		</main><!-- #main -->
     </div><!-- #primary -->
                 </div>
 </div>
+<?php get_sidebar(); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
